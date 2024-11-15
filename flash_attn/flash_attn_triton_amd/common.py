aec51abaca9b9f34a6e0ed75360a3820393bfe57
Michael Melesse <micmelesse@gmail.com>
2024-11-16 03:14:06 +0530

    simple droput out mask

--- flash_attn/flash_attn_triton_amd/common.py
+++ flash_attn/flash_attn_triton_amd/common.py
@@ -6,22 +6,17 @@ def cdiv_fn(x, y):
     return (x + y - 1) // y
 
 @triton.jit
-def dropout_offsets(philox_seed, philox_offset, dropout_p, m, n, stride):
-    # tl.device_print('philox_offset:', philox_offset)
+def dropout_mask(philox_seed, philox_offset, dropout_p, m, n, stride):
+    # calculate RNG offsets using the philox_offset and strides
     ms = tl.arange(0, m)
     ns = tl.arange(0, n)
-    return philox_offset + ms[:, None] * stride + ns[None, :]
+    rng_offsets = (philox_offset + ms[:, None] * stride + ns[None, :]).to(tl.uint32)
+    
+    # get rng output
+    rng_output = tl.rand(philox_seed, rng_offsets) # TODO: use tl.randint for better performance
 
-
-@triton.jit
-def dropout_rng(philox_seed, philox_offset, dropout_p, m, n, stride):
-    rng_offsets = dropout_offsets(philox_seed, philox_offset, dropout_p, m, n, stride).to(tl.uint32)
-    # TODO: use tl.randint for better performance
-    return tl.rand(philox_seed, rng_offsets)
-
-
-@triton.jit
-def dropout_mask(philox_seed, philox_offset, dropout_p, m, n, stride):
-    rng_output = dropout_rng(philox_seed, philox_offset, dropout_p, m, n, stride)
+    
+    # keep 1 - dropout elements
     rng_keep = rng_output > dropout_p
+
     return rng_keep
